616b2064eec1458020fd6b75c404b851646a4a77
mokuo <mokuo@dataseek.info>
2022-12-11 06:26:44 +0800

    feat(#N/A): add hover info for CID

--- src/decorator.ts
+++ src/decorator.ts
@@ -1,3 +1,4 @@
+import { CID } from 'multiformats/cid'
 import * as vscode from 'vscode'
 import { DAEMONE_ON, DAEMON_STATUS } from './constants'
 
@@ -13,16 +14,40 @@ export const decorate = (context: vscode.ExtensionContext) => {
   let match
   let decorators: vscode.DecorationOptions[] = []
   let text = editor.document.getText()
-  let regex = /(北京)|(沧海)|(夕)|(兮)|(十年生死)|(茫茫)|(不思量)|(自难忘)/g
+  let cidRegExp =
+    /(Qm[1-9A-HJ-NP-Za-km-z]{44})|(b[A-Za-z2-7]{58})|(B[A-Z2-7]{58})|(z[1-9A-HJ-NP-Za-km-z]{48})|(F[0-9A-F]{50})/g
 
-  while ((match = regex.exec(text)) && daemonStatus == DAEMONE_ON) {
+  while ((match = cidRegExp.exec(text)) && daemonStatus == DAEMONE_ON) {
     const startPos = editor.document.positionAt(match.index)
     const endPos = editor.document.positionAt(match.index + match[0].length)
-    const decoration = {
-      range: new vscode.Range(startPos, endPos),
-      hoverMessage: ['which match' + match[0], 'test']
+    let cid: CID
+    try {
+      cid = CID.parse(match[0])
+    } catch (error: any) {
+      console.error(`Parse CID(${match[0]}) failed: ${error.message}`)
+      continue
     }
-    decorators.push(decoration)
+    decorators.push({
+      range: new vscode.Range(startPos, endPos),
+      hoverMessage: buildCidHoverMessage(cid)
+    })
   }
   editor.setDecorations(style, decorators)
 }
+
+const buildCidHoverMessage = (cid: CID): vscode.MarkdownString =>
+  new vscode.MarkdownString(
+    `**IPFS CID**
+
+---
+
+Version: \`${cid.version}\`
+
+Code: \`${toHexString(cid.code)}\`
+
+Multihash:
+  - Code: \`${toHexString(cid.multihash.code)}\`
+  - Digest (hex): \`${Buffer.from(cid.multihash.digest).toString('hex').toUpperCase()}\``
+  )
+
+const toHexString = (n: number): string => `0x${n.toString(16).toUpperCase()}`
